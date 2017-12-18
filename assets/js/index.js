b82379e322e422d9c6666bf305db8c070fb8856c
piordanov <iordano2@illinois.edu>
2017-12-17 23:49:03 -0600

    working with tachyons

---
+++ assets/js/index.js
@@ -0,0 +1,2 @@
+console.log("Hello World! \nI'm glad you've taken an interest into my site!\nI'm sure you can find your way around, but it could always be easier to see the actual files on my github:\nhttps://github.com/piordanov/piordanov.github.io");
+
